2c634c843e379d4a49af9587cc0346a0876a182d
andpli <pandre@tut.by>
2023-08-28 13:56:50 +0300

    fead: guesssing game ready

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
+let dMax = 0;
+let dMin = 0;
+let dRes = 0;
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+    dMax = max;
+    dMin = min;
     }
 
     guess() {
-
+    dRes = Math.ceil((dMax + dMin)/ 2);
+    return dRes;
     }
 
     lower() {
-
+       dMax = dRes;
     }
 
     greater() {
-
+        dMin = dRes;
     }
 }
 
